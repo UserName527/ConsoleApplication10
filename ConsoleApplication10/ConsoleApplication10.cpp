e69df58eb795b3767fb47b559088fdf751662c6b
Atos <70628905+ireadyium@users.noreply.github.com>
2023-10-06 10:21:18 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApplication10/ConsoleApplication10.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    std::cout << "Введите оценку:";
+    int grade;
+    std::cin >> grade;
+    switch (grade) {
+        case 5:
+            std::cout << "отлично";
+            break;
+        case 4: 
+            std::cout << "хорошо";
+            break;
+        case 3:
+            std::cout << "средне";
+        default: 
+            std::cout << "хуже среднего";
+            
+    }
+}
